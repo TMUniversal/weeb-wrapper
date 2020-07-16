cca8e8757e1972e12d2455b194e497cf2d5b8460
Universal Studio <uni@un1.pw>
2020-07-16 04:45:35 +0200

    Update repository

---
+++ .github/CONTRIBUTING.md
@@ -0,0 +1,20 @@
+# Contribution Rules
+
+## Minimum requirements for contributing
+
+_You do not have to be a master on the following, but at least have general knowledge and understanding of the following concepts:_
+
+- [APIs]
+- [JavaScript]
+- [axios]
+
+## _Before opening a Pull Request_
+
+1. Ask yourself if that will be useful for the project.
+   - Either code wise or to the end user.
+2. Check if your code works.
+3. Follow this repository's Code Of Conduct.
+
+[APIs]: https://en.wikipedia.org/wiki/Application_programming_interface
+[javascript]: https://en.wikipedia.org/wiki/JavaScript
+[axios]: https://www.npmjs.com/package/axios
--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-'use strict';
+'use strict'
 
-const Iroh = require('./lib/iroh/');
-const Toph = require('./lib/toph');
+const Iroh = require('./lib/iroh/')
+const Toph = require('./lib/toph')
 
 const apis = {
-    PRODUCTION: 'https://api.weeb.sh',
-    STAGING: 'https://staging.weeb.sh',
-};
-
-class WeebServices {
-    constructor(token, apiUrl) {
-        apiUrl = apiUrl || {};
-
-        let accountsUrl;
-        let imagesUrl;
-        if (typeof apiUrl === 'string') {
-            accountsUrl = `${apiUrl}/accounts`;
-            imagesUrl = `${apiUrl}/images`;
-        } else {
-            accountsUrl = apiUrl.accounts || `${apis.PRODUCTION}/accounts`;
-            imagesUrl = apiUrl.images || `${apis.PRODUCTION}/images`;
-        }
-
-        this.Accounts = new Iroh(accountsUrl, token);
-        this.Images = new Toph(imagesUrl, token);
-    }
+  PRODUCTION: 'https://api.tmuniversal.eu'
 }
 
-WeebServices.APIS = apis;
+class WeebServices {
+  constructor (token, apiUrl) {
+    apiUrl = apiUrl || {}
 
-module.exports = WeebServices;
+    let accountsUrl
+    let imagesUrl
+    if (typeof apiUrl === 'string') {
+      accountsUrl = `${apiUrl}/accounts`
+      imagesUrl = `${apiUrl}/images`
+    } else {
+      accountsUrl = apiUrl.accounts || `${apis.PRODUCTION}/accounts`
+      imagesUrl = apiUrl.images || `${apis.PRODUCTION}/images`
+    }
+
+    this.Accounts = new Iroh(accountsUrl, token)
+    this.Images = new Toph(imagesUrl, token)
+  }
+}
+
+WeebServices.APIS = apis
+
+module.exports = WeebServices
